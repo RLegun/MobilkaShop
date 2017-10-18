ff0f0ca28f5dfd8abdbc90718565a11d2546e6c3
Роман <legrom@ukr.net>
2017-10-18 21:44:31 +0300

    training

--- src/main/java/Shop/App.java
+++ src/main/java/Shop/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!" );
-        Cart cart = new Cart(1, "xxx", 2, 4, 25);
+        Cart cart = new Cart(1, "test", 250, 4, 25);
 
         System.out.println(cart.getNameOfProduct());
     }
